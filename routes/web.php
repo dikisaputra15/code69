a8b76ba03a4b19ace6d0fd874b8ab2bf5d0439a6
Diki Saputra <diki@CNCRD.LAN>
2024-06-13 17:05:19 +0700

    blade template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('pages.auth.loginadmin');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+});
